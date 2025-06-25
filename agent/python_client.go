130c66d8fd8e5af0cc5b070c85fbec002bbfed25
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-06-25 10:41:17 -0400

    Update python_client.go

--- agent/python_client.go
+++ agent/python_client.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -12,11 +13,26 @@ type PythonClient struct {
 	reader *bufio.Reader
 }
 
+// Strips the port from host if it's present, e.g. "192.168.0.9:8080" => "192.168.0.9"
+func stripPortIfNeeded(host string) string {
+	if strings.Contains(host, ":") {
+		strippedHost, _, err := net.SplitHostPort(host)
+		if err == nil {
+			return strippedHost
+		}
+	}
+	return host
+}
+
 func NewPythonClient(host string, port int) (*PythonClient, error) {
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", host, port), 5*time.Second)
+	cleanHost := stripPortIfNeeded(host)
+	fullAddr := fmt.Sprintf("%s:%d", cleanHost, port)
+
+	conn, err := net.DialTimeout("tcp", fullAddr, 5*time.Second)
 	if err != nil {
 		return nil, err
 	}
+
 	return &PythonClient{
 		conn:   conn,
 		reader: bufio.NewReader(conn),
